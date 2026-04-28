6f5b97f42ec93c68431c367da3107f64cd15ca61
Darleanow <enzohugonn@gmail.com>
2026-04-28 12:19:56 +0200

    chore(app.module): Update code

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { EventEmitterModule } from '@nestjs/event-emitter';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { IdentityModule } from './modules/identity/identity.module';
 import { CatalogModule } from './modules/catalog/catalog.module';
 import { AdaptiveModule } from './modules/adaptive/adaptive.module';
@@ -9,8 +11,25 @@ import { CertificationModule } from './modules/certification/certification.modul
 import { TenantModule } from './modules/tenant/tenant.module';
 
 @Module({
-  imports: [IdentityModule, CatalogModule, AdaptiveModule, AssessmentModule, CertificationModule, TenantModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    EventEmitterModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME,
+      autoLoadEntities: true,
+      synchronize: true, // TO DISABLE
+    }),
+    TenantModule,
+    IdentityModule,
+    CatalogModule,
+    AdaptiveModule,
+    AssessmentModule,
+    CertificationModule,
+  ],
 })
 export class AppModule {}
